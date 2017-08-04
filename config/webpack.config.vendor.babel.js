24f0cc87bc9b0d05aeb7f3a1636f004506ff837d
Jordan McArdle <jordanmcardle@gmail.com>
2017-08-04 01:54:24 -0500

    Redux branch init with the webpack vendor file.  Still need to actually implement redux and call GITHUB api as an example :)

--- config/webpack.config.vendor.babel.js
+++ config/webpack.config.vendor.babel.js
@@ -1,6 +1,5 @@
 import path from 'path'
 import webpack from 'webpack'
-import BabiliPlugin from 'babili-webpack-plugin'
 
 import PATHS from './paths.babel'
 
@@ -8,21 +7,29 @@ const vendorConfig = {
   context: process.cwd(),
   devtool: '#source-map',
   entry: [
+    // React
     'react',
     'react-dom',
     'prop-types',
+
+    // Redux
+    'redux',
+    'redux-immutable',
+    'redux-api-middleware',
+
+    // Libraries
     'promise'
   ],
   output: {
     filename: 'vendors.dll.js',
     library: 'vendors_lib',
-    path: path.resolve(process.cwd(), 'dll')
+    path: path.resolve(PATHS.root, 'dll')
   },
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.DllPlugin({
       name: 'vendors_lib',
-      path: path.resolve(process.cwd(), 'dll/vendors-manifest.json')
+      path: path.resolve(PATHS.root, 'dll/vendors-manifest.json')
     })
   ],
   node: {
