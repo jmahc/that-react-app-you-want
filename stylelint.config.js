4de2d47c884eba22beba890684ddb2b9b6cfdcf8
Jordan McArdle <jordanmcardle@gmail.com>
2020-07-15 12:25:28 -0500

    Removing stylelint.

--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  configBasedir: './',
-  extends: 'stylelint-config-recommended', //,
-  // plugins: ['stylelint-scss'],
-  // rules: {
-  //   'double-slash-comment-inline': 'never'
-  // }
-}
